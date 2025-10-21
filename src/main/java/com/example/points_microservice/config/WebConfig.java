ebda7936be7db8fc21aa0fc6ee825da951d70143
lesg1151860 <luiseduardosg@ufps.edu.co>
2025-10-21 16:43:07 -0500

    feat: adding endpoint for point consult

---
+++ src/main/java/com/example/points_microservice/config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.example.points_microservice.config;
+
+public class WebConfig {
+}
--- src/main/java/com/example/points_microservice/repository/PuntosRepository.java
+++ src/main/java/com/example/points_microservice/repository/PuntosRepository.java
@@ -3,5 +3,8 @@ package com.example.points_microservice.repository;
 import com.example.points_microservice.model.Puntos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PuntosRepository extends JpaRepository<Puntos, Long> {
+    Optional<Puntos> findByEmail(String email);
 }
